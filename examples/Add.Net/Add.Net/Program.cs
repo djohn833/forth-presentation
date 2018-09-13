2a02d4b62bfea23d0ea75906f1975b5e1be20d03
Dave Johnson <dave.al.johnson@gmail.com>
2018-09-12 22:16:28 -0400

    Initial commit.

---
+++ examples/Add.Net/Add.Net/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// ReSharper disable once CheckNamespace
+class Program
+{
+    public static int Add(int a, int b) => a + b;
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("1 + 2 = {0}", Add(1, 2));
+    }
+}
---
+++ examples/add.wat
@@ -0,0 +1,10 @@
+(module
+  (type (;0;) (func (param i32 i32) (result i32)))
+  (func (;0;) (type 0) (param i32 i32) (result i32)
+    get_local 1
+    get_local 0
+    i32.add)
+  (table (;0;) 0 anyfunc)
+  (memory (;0;) 1)
+  (export "memory" (memory 0))
+  (export "_Z3addii" (func 0)))
